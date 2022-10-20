776354df792defd218740492cb925bdb07041041
Jacek Stan <jacek.stanusz@gmail.com>
2022-10-20 09:38:51 +0700

    change database data

--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -3,12 +3,34 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/urldb")
+	var db_user string
+	if os.Getenv("DB_USER") == "" {
+		db_user = "root"
+	} else {
+		db_user = os.Getenv("DB_USER")
+	}
+
+	var db_pass string
+	if os.Getenv("DB_PASS") == "" {
+		db_pass = ""
+	} else {
+		db_pass = os.Getenv("DB_PASS")
+	}
+
+	var db_name string
+	if os.Getenv("DB_NAME") == "" {
+		db_name = "arbitrage"
+	} else {
+		db_name = os.Getenv("DB_NAME")
+	}
+
+	db, err := sql.Open("mysql", db_user+":"+db_pass+"@tcp(127.0.0.1:3306)/"+db_name)
 	if err != nil {
 		panic(err.Error())
 	}
